e6f3b15ea350ffd97638ca73c7cf7a08b5ead23f
Sophie van Soest <sophie@entropie.rocks>
2020-12-02 07:19:11 +0100

    Removed unused line from input

--- utils/input.py
+++ utils/input.py
@@ -11,7 +11,6 @@ def load_input(file_path, input_type):
     cprint(f"Loading input file: {file_path} with type: {input_type.name}", "white")
     with open(file_path) as file:
         lines = file.read().splitlines()
-        results = list(map(int, lines))
         if input_type == Type.STRING:
             results = lines
         elif input_type == Type.INT:
