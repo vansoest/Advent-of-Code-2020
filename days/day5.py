5d16d8b495cc5169077005bfb431756cc3347b93
Sophie van Soest <sophie@entropie.rocks>
2020-12-05 14:51:18 +0100

    Added day5

---
+++ days/day5.py
@@ -0,0 +1,39 @@
+from re import findall, match
+from copy import deepcopy
+from functools import reduce
+from termcolor import cprint, colored
+from utils.input import Type as InputType
+
+input_type = InputType.STRING
+
+
+def calc(line: str, is_column: bool) -> tuple:
+    start, end = (0, 7 if is_column else 127)
+    related_letters = line[-3:] if is_column else line[0:7]
+
+    for letter in related_letters:
+        start, end = (
+            (start, (end + start) // 2)
+            if letter == "F" or letter == "L"
+            else ((start + end) // 2 + 1, end)
+            if letter == "B" or letter == "R"
+            else (start, end)
+        )
+
+    return start
+
+
+def run(input: list, verbose: bool) -> tuple:
+    part1 = None
+    part2 = None
+
+    results = list()
+    for line in input:
+        row, column = calc(line, is_column=False), calc(line, is_column=True)
+        seat_id = row * 8 + column
+        results.append(seat_id)
+
+    part2 = reduce(lambda a, b: a if a != (b - 1) else b, sorted(results)) + 1
+
+    part1 = max(results)
+    return part1, part2
